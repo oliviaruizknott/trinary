0bc44361b5800789f429695f3cb4f1fcf2868484
oliviaruizknott <olivia@ruiz-knott.com>
2021-09-11 17:16:12 -0600

    set up for future instances

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import React, { useState } from 'react'
-import Diagram from './Diagram'
+import Trinary from './Trinary'
 
 function App () {
-  const [string, setString] = useState("")
-  const [color1, setColor1] = useState("#808080") // gray
-  const [color2, setColor2] = useState("#000000") // black
-  const colors = ["FFFFFF", color1, color2]
-
-  const swapColors = () => {
-    const newColor1 = color2
-    const newColor2 = color1
-
-    setColor1(newColor1)
-    setColor2(newColor2)
-  }
-
   return (
     <div className="App">
-      <input type="text" name="string" onChange={(e) => setString(e.target.value)} placeholder="enter text here"/>
-      <br />
-      <input type="text" name="color1" style={{ color: color1 }} onChange={(e) => setColor1(e.target.value)} value={color1} />
-      <input type="text" name="color2" style={{ color: color2 }} onChange={(e) => setColor2(e.target.value)} value={color2} />
-      <button type="button" name="swap" onClick={swapColors}>swap</button>
-      <Diagram string={string} colors={colors} />
+      <Trinary />
     </div>
   )
 }
---
+++ src/Trinary.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import Diagram from './Diagram'
+
+function Trinary ({ id }) {
+  const [string, setString] = useState("")
+  const [color1, setColor1] = useState("#808080") // gray
+  const [color2, setColor2] = useState("#000000") // black
+  const colors = ["FFFFFF", color1, color2]
+
+  const swapColors = () => {
+    const newColor1 = color2
+    const newColor2 = color1
+
+    setColor1(newColor1)
+    setColor2(newColor2)
+  }
+
+  return (
+    <div id={id} className="Trinary">
+      <input type="text" name="string" onChange={(e) => setString(e.target.value)} placeholder="enter text here"/>
+      <br />
+      <input type="text" name="color1" style={{ color: color1 }} onChange={(e) => setColor1(e.target.value)} value={color1} />
+      <input type="text" name="color2" style={{ color: color2 }} onChange={(e) => setColor2(e.target.value)} value={color2} />
+      <button type="button" name="swap" onClick={swapColors}>swap</button>
+      <br />
+      <Diagram string={string} colors={colors} />
+    </div>
+  )
+}
+
+export default Trinary;
