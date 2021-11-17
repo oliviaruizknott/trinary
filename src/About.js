ed8dbf9b1b2cfafa3da1dae5c0c9a26c96cb7c41
oliviaruizknott <olivia@ruiz-knott.com>
2021-11-16 20:25:54 -0700

    add custom monospace font

--- src/About.js
+++ src/About.js
@@ -3,7 +3,6 @@ import Letter from './Letter'
 import { convertToNumber, numberInTrinaryObject } from './utils'
 
 function About ({ closeAbout }) {
-  const [showAbout, setShowAbout] = useState(false)
   const [string, setString] = useState(" ")
   const colors = ["FFFFFF", "#808080", "#000000"]
 
@@ -13,14 +12,13 @@ function About ({ closeAbout }) {
 
   return (
     <div className="About">
-      <p>Hi! I’m Livvy.</p>
       <p>I made this one weekend while trying to come up with abstract-looking artwork that could hold secret meaning. Maybe one day I’ll write a message and use this to design a massive tapestry to hang on our wall, and you’ll be in on it.</p>
       <p>Here’s how it works:</p>
       <input type="text" name="string" onChange={(e) => setString(e.target.value)} placeholder="type any letter here" maxLength="1"/>
       <p>Each letter you enter is converted to a number: A = 1, B = 2, C = 3, all the way down to Z = 26. So your letter becomes:</p>
       <p>{convertToNumber(string, true)}</p>
-      <p>That number is then converted into it’s base 3 representation. What does that mean? You and I are used to a base 10 number system: it starts at zero, walks all the way up to nine--and then you put a one in a new column (the "tens place") to say that there’s one ten here, plus whatever other number is in the "ones place", where you start all over at zero.</p>
-      <p>A base 3 system is the same, except with threes! It starts at zero, walks up to 2--and then you put a one in a new column (the "threes place") to say that there’s one three here, plus whatever other number is in the "ones place", where you start all over at zero. Just like ten tens make one hundred and you have to start a new column, three threes make a nine and you start a new column, too.</p>
+      <p>That number is then converted into it’s base 3 representation. What does that mean? You and I are used to a base 10 number system: it starts at zero, walks all the way up to nine—and then you put a one in a new column (the “tens place”) to say that there’s one ten here, plus whatever other number is in the “ones place”, where you start all over at zero.</p>
+      <p>A base 3 system is the same, except with threes! It starts at zero, walks up to two—and then you put a one in a new column (the “threes place”) to say that there’s one three here, plus whatever other number is in the “ones place”, where you start all over at zero. Just like ten tens make one hundred and you have to start a new column, three threes make a nine and you start a new column, too.</p>
       <p>So your letter-turned-number becomes:</p>
       <p>{trinaryAsNumber(numberInTrinaryObject(convertToNumber(string)))}</p>
       <p>or:</p>
@@ -63,7 +61,8 @@ function About ({ closeAbout }) {
       }
       <p>Great job!</p>
       <p>Now go back and type all sorts of words and sentences and see what shapes they make.</p>
-      <p>(Some of my favorites so far are: "december", "twelve", "twenty", "fourteen", "notion", and of course "livvy".)</p>
+      <p>(Some of my favorites so far are: “december”, “twelve”, “twenty”, “fourteen”, “notion”, and of course “livvy”.)</p>
+      <p>Conceptualized, designed, and built by Livvy. Typeface by Ivan. A project of <a href="https://ruiz-knott.com" className="clickable">Ruiz & Knott.</a></p>
       <p>P.S. If you like this, you might like <a className="clickable" href="https://ruiz-knott.notion.site/obscured_data-aae285e2f7524780a4c92ec40eb1b6e8" target="_blank" rel="noopener noreferrer">@obscured_data.</a></p>
     </div>
   )
